3e9ca68162b38a5ee9bafbf01b4ba68c96e418b8
thmeme <thierrymeme@gmail.com>
2017-04-17 22:29:12 +0200

    google map

--- app/js/controllers/dashboardController.js
+++ app/js/controllers/dashboardController.js
@@ -1,6 +1,7 @@
 angular.module('app')
-    .controller('DashboardController', function($scope, CurrentUser, UserService) {
+    .controller('DashboardController', function($scope, CurrentUser, UserService, NgMap) {
         UserService.getOne(CurrentUser.user()._id).then(function(res) {
             $scope.user = res.data;
         });
+        
     });
